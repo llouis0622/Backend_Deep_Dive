466ec4b16c3b3be3b9b59963bcfa3be5d582660d
LLouis <llouis0622@gmail.com>
2024-05-19 14:43:22 +0900

    .

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,14 +1,14 @@
 package hello.proxy;
 
+import hello.proxy.config.v6_aop.AopConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 
-@Import(DynamicProxyFilterConfig.class)
+@Import(AopConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
     public static void main(String[] args) {
