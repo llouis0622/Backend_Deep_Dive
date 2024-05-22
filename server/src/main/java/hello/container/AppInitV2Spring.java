7fa715c3aef58b1015fd46c41d0a48d2c083d804
LLouis <llouis0622@gmail.com>
2024-05-22 10:24:17 +0900

    .

---
+++ server/src/main/java/hello/container/AppInitV2Spring.java
@@ -0,0 +1,22 @@
+package hello.container;
+
+import hello.spring.HelloConfig;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletRegistration;
+
+public class AppInitV2Spring implements AppInit {
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        System.out.println("AppInitV2Spring.onStartup");
+
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.register(HelloConfig.class);
+
+        DispatcherServlet dispatcher = new DispatcherServlet(appContext);
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcherV2", dispatcher);
+
+        servlet.addMapping("/spring/*");
+    }
+}
