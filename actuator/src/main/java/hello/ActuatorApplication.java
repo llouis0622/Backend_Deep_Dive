9617a7ab944ac71afeb55bfc17f98091c7e3c7b8
LLouis <llouis0622@gmail.com>
2024-05-24 00:46:59 +0900

    .

---
+++ actuator/src/main/java/hello/ActuatorApplication.java
@@ -0,0 +1,22 @@
+package hello;
+
+import hello.order.gauge.StockConfigV2;
+import hello.order.v4.OrderConfigV4;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+@Import({OrderConfigV4.class, StockConfigV2.class})
+@SpringBootApplication(scanBasePackages = "hello.controller")
+public class ActuatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+    @Bean
+    public InMemoryHttpExchangeRepository httpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
+    }
+}
